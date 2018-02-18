2e8cb62b3aa863faae32c41a0992bcb01d54800c
Nenguou <zhengzuoyu0414@gmail.com>
2018-02-19 02:52:18 +0800

    finish database

--- app/src/main/java/com/nenguou/dayuandaily/DataBase/DayuanDailyDatabase.java
+++ app/src/main/java/com/nenguou/dayuandaily/DataBase/DayuanDailyDatabase.java
@@ -2,6 +2,7 @@ package com.nenguou.dayuandaily.DataBase;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
@@ -20,6 +21,11 @@ import java.util.regex.Pattern;
  */
 
 public class DayuanDailyDatabase {
+
+    public static final int TYPE_GET_SUB_NAME = 1;
+    public static final int TYPE_GET_SUB_PLACE = 2;
+    public static final int TYPE_GET_SUB_TEACHER = 3;
+    public static final int TYPE_GET_SUB_TIME = 4;
 
     public static final String DB_NAME = "Dayuan_database";
     public static final int DB_VERSION = 1;
@@ -58,11 +64,19 @@ public class DayuanDailyDatabase {
     public void saveMajor(Major major) {
         if (major != null) {
             ContentValues contentValues = new ContentValues();
-            contentValues.put("major_id", major.getData().get(0).getId());
-            contentValues.put("major_name", major.getData().get(0).getMajor());
-            contentValues.put("college_id", major.getData().get(0).getCollegeId());
-            sqLiteDatabase.insert("Major", null, contentValues);
-            contentValues.clear();
+            for(Major.DataBean dataBean: major.getData()){
+                try {
+                    if(!isExitThisMajor(dataBean.getId())){
+                        contentValues.put("major_id", dataBean.getId());
+                        contentValues.put("major_name", dataBean.getMajor());
+                        contentValues.put("college_id", dataBean.getCollegeId());
+                        sqLiteDatabase.insert("Major", null, contentValues);
+                        contentValues.clear();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
@@ -70,13 +84,19 @@ public class DayuanDailyDatabase {
         if (className != null) {
             List<String> data = className.getData();
             ContentValues contentValues = new ContentValues();
-            for (String s : data) {
-                //Log.d("DaYuanTag",s+"");
-                contentValues.put("className", s);
-                contentValues.put("major_name", major_name);
-                contentValues.put("year", year);
-                sqLiteDatabase.insert("ClassName", null, contentValues);
-                contentValues.clear();
+            try {
+                if(!isExitThisClassName(year,major_name)) {
+                    for (String s : data) {
+                        //Log.d("DaYuanTag",s+"");
+                        contentValues.put("className", s);
+                        contentValues.put("major_name", major_name);
+                        contentValues.put("year", year);
+                        sqLiteDatabase.insert("ClassName", null, contentValues);
+                        contentValues.clear();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
@@ -117,23 +137,29 @@ public class DayuanDailyDatabase {
                         while (matcher1.find()) {
                             //Log.d("WocaoNiMaTag122",matcher2.groupCount()+" 个");
                             int numberClass = Integer.parseInt(matcher1.group(2)) + 1;
-                            Log.d("WocaoNiMaTag123","周 "+numberWeek+"  第 "+ numberClass +" 节课  "+"课程名字: "+matcher1.group(3)+"  教学楼:"+matcher1.group(4) +
-                            "  教室：" + matcher1.group(5) + "  教师姓名：" + matcher1.group(6) + "教学时间: " + matcher1.group(7));
-                            contentValues.put("schedule_id",mClass.getData().getId());
-                            contentValues.put("term",mClass.getData().getTerm());
-                            contentValues.put("year",mClass.getData().getYear());
-                            contentValues.put("class_name",mClass.getData().getName());//班级名称 eg 软件 1632
-                            contentValues.put("college_name",mClass.getData().getCollege());//学院名称 eg 软件学院
-                            contentValues.put("major_name",mClass.getData().getMajor());//专业名称 eg 软件工程
-                            contentValues.put("week_num",numberWeek);
-                            contentValues.put("course_num",numberClass);
-                            contentValues.put("course_name",matcher1.group(3));
-                            contentValues.put("course_place",matcher1.group(4));
-                            contentValues.put("course_detial_place",matcher1.group(5));
-                            contentValues.put("teacher_name",matcher1.group(6));
-                            contentValues.put("course_time",matcher1.group(7));
-                            sqLiteDatabase.insert("Schedule",null,contentValues);
-                            contentValues.clear();
+                            //Log.d("WocaoNiMaTag123","周 "+numberWeek+"  第 "+ numberClass +" 节课  "+"课程名字: "+matcher1.group(3)+"  教学楼:"+matcher1.group(4) +
+                            //"  教室：" + matcher1.group(5) + "  教师姓名：" + matcher1.group(6) + "教学时间: " + matcher1.group(7));
+                            try {
+                                if(!isExitThisSchedule(mClass.getData().getId())) {
+                                    contentValues.put("schedule_id", mClass.getData().getId());
+                                    contentValues.put("term", mClass.getData().getTerm());
+                                    contentValues.put("year", mClass.getData().getYear());
+                                    contentValues.put("class_name", mClass.getData().getName());//班级名称 eg 软件 1632
+                                    contentValues.put("college_name", mClass.getData().getCollege());//学院名称 eg 软件学院
+                                    contentValues.put("major_name", mClass.getData().getMajor());//专业名称 eg 软件工程
+                                    contentValues.put("week_num", numberWeek);
+                                    contentValues.put("course_num", numberClass);
+                                    contentValues.put("course_name", matcher1.group(3));
+                                    contentValues.put("course_place", matcher1.group(4));
+                                    contentValues.put("course_detial_place", matcher1.group(5));
+                                    contentValues.put("teacher_name", matcher1.group(6));
+                                    contentValues.put("course_time", matcher1.group(7));
+                                    sqLiteDatabase.insert("Schedule", null, contentValues);
+                                    contentValues.clear();
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
                     }
                     //Log.d("WocaoNiMaTag1","=========================");
@@ -141,5 +167,162 @@ public class DayuanDailyDatabase {
             }
 
         }
+    }
+
+    /**
+     * 返回所有学院的数据
+     */
+    public List<YearCollege.DataBean.CollegesBean> loadYearCollege(){
+        List<YearCollege.DataBean.CollegesBean> list = new ArrayList<>();
+        Cursor cursor = sqLiteDatabase.query("YearCollege",null,null,null,null,null,null);
+        if(cursor.moveToFirst()){
+            do{
+                YearCollege.DataBean.CollegesBean collegesBean = new YearCollege.DataBean.CollegesBean();
+                collegesBean.setId(cursor.getInt(cursor.getColumnIndex("college_id")));
+                collegesBean.setCollege(cursor.getString(cursor.getColumnIndex("college_name")));
+                list.add(collegesBean);
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        return list;
+    }
+
+    /**
+     * 判断表是否存在这个学院
+     * @return
+     */
+    public boolean isExitThisCollege(){
+        return false;
+    }
+
+    /**
+     * @param collegeId 根据传入的 collegeId 获得该学院的所有专业
+     * @return 该学院的专业集合
+     */
+    public List<Major.DataBean> loadMajor(int collegeId){
+        List<Major.DataBean> list = new ArrayList<>();
+        Cursor cursor = sqLiteDatabase.query("Major",null,"college_id = ?",new String[]{String.valueOf(collegeId)},null,null,null);
+        if(cursor.moveToFirst()){
+            do{
+                Major.DataBean dataBean = new Major.DataBean();
+                dataBean.setCollegeId(cursor.getInt(cursor.getColumnIndex("college_id")));
+                dataBean.setId(cursor.getInt(cursor.getColumnIndex("major_id")));
+                dataBean.setMajor(cursor.getString(cursor.getColumnIndex("major_name")));
+                list.add(dataBean);
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        return list;
+    }
+
+    /**
+     * @param id 根据传入的 专业 ID 判断表中是否存在这节课
+     * @return 存在返回 true，不存在返回 false；
+     */
+    public boolean isExitThisMajor(int id) throws Exception {
+        Cursor cursor = sqLiteDatabase.query("Major",null,"major_id = ?",new String[]{String.valueOf(id)},null,null,null);
+        if(cursor.moveToFirst()){
+            int mID = cursor.getInt(cursor.getColumnIndex("major_id"));
+            if(mID == id){
+                cursor.close();
+                return true;
+            }
+        }
+        cursor.close();
+        return false;
+    }
+
+    /**
+     * @param year 专业的学年
+     * @param major_name 专业名称
+     * @return 返回该专业该学年的所有班级
+     */
+    public List<String> loadClassName(int year, String major_name){
+        List<String> list = new ArrayList<>();
+        Cursor cursor = sqLiteDatabase.query("ClassName",null,"year = ? and major_name like ?",new String[]{String.valueOf(year),major_name},null,null,null);
+        if(cursor.moveToFirst()){
+            do{
+                String s = cursor.getString(cursor.getColumnIndex("className"));
+                list.add(s);
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        return list;
+    }
+
+    /**
+     * @param year 这些班级的入学年日期
+     * @param major_name 这些班级的专业名字
+     * @return 如果表中存在这一年这一专业的数据，返回 true
+     * @throws Exception 返回「不知道什么」异常
+     */
+    public boolean isExitThisClassName(int year,String major_name) throws Exception{
+        Cursor cursor = sqLiteDatabase.query("ClassName",null,"year = ? and major_name like ?",new String[]{String.valueOf(year),major_name},null,null,null);
+        if(cursor.moveToFirst()){
+            do{
+                String mString = cursor.getString(cursor.getColumnIndex("major_name"));
+                int mInt = cursor.getInt(cursor.getColumnIndex("year"));
+                if(year == mInt && major_name.equals(mString)){
+                    cursor.close();
+                    return true;
+                }
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        return false;
+    }
+
+    /**
+     * @param Type TYPE_GET_SUB_NAME = 1; TYPE_GET_SUB_PLACE = 2;TYPE_GET_SUB_TEACHER = 3;TYPE_GET_SUB_TIME = 4;
+     * @param schedule_id 课表的唯一 id
+     * @param week_num 课表的周几
+     * @param course_num 课表的周几的第几节
+     * @return 根据 Type 不同，分别返回课程名字，上课地点，上课老师，上课的周数
+     */
+    public String loadSchedules(int Type,int schedule_id,int week_num,int course_num){
+        //List<String> list = new ArrayList<>();
+        String s = null;
+        Cursor cursor = sqLiteDatabase.query("Schedule",null,"schedule_id = ? and week_num = ? and course_num = ?",
+                        new String[]{String.valueOf(schedule_id),String.valueOf(week_num),String.valueOf(course_num)},null,null,null);
+        if(cursor.moveToFirst()){
+            do{
+                switch (Type){
+                    case DayuanDailyDatabase.TYPE_GET_SUB_NAME:
+                        s = cursor.getString(cursor.getColumnIndex("course_name"));
+                        break;
+                    case DayuanDailyDatabase.TYPE_GET_SUB_PLACE:
+                        s = cursor.getString(cursor.getColumnIndex("course_place"))+" "+cursor.getString(cursor.getColumnIndex("course_detial_place"));
+                        break;
+                    case DayuanDailyDatabase.TYPE_GET_SUB_TEACHER:
+                        s = cursor.getString(cursor.getColumnIndex("teacher_name"));
+                        break;
+                    case DayuanDailyDatabase.TYPE_GET_SUB_TIME:
+                        s = cursor.getString(cursor.getColumnIndex("course_time"));
+                        break;
+                }
+            }while (cursor.moveToNext());
+        }
+        return s;
+
+    }
+
+    /**
+     * @param schedule_id 传入课表的唯一 id
+     * @return 如果数据库中存在这个课表，返回为 true
+     * @throws Exception 「不知道什么」异常
+     */
+    public boolean isExitThisSchedule(int schedule_id) throws Exception{
+        Cursor cursor = sqLiteDatabase.query("Schedule",null,"schedule_id = ?",new String[]{String.valueOf(schedule_id)},null,null,null);
+        if(cursor.moveToFirst()){
+            do {
+                int id = cursor.getInt(cursor.getColumnIndex("schedule_id"));
+                if(id == schedule_id){
+                    cursor.close();
+                    return true;
+                }
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        return false;
     }
 }
--- app/src/main/java/com/nenguou/dayuandaily/MainActivity.java
+++ app/src/main/java/com/nenguou/dayuandaily/MainActivity.java
@@ -2,15 +2,23 @@ package com.nenguou.dayuandaily;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.nenguou.dayuandaily.DataBase.DayuanDailyDatabase;
+import com.nenguou.dayuandaily.Model.Major;
+import com.nenguou.dayuandaily.Model.YearCollege;
 import com.nenguou.dayuandaily.Utils.RxDayuan;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button test_YearCollege,test_Major,test_classname,test_Class;
+    private Button test_YearCollege,test_Major,test_classname,test_Class,test_loadYearColleg,test_loadMajor,test_loadClassName,test_loadClass;
     private RxDayuan rxDayuan;
+    private final String mainTag = "MainActivityTag";
+    DayuanDailyDatabase dayuanDailyDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,14 +38,14 @@ public class MainActivity extends AppCompatActivity {
         test_Major.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                rxDayuan.getMajor(22);
+                rxDayuan.getMajor(12);
             }
         });
 
         test_classname.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                rxDayuan.getClassName(2017,"软件工程");
+                rxDayuan.getClassName(2016,"工业设计");
             }
         });
 
@@ -47,13 +55,56 @@ public class MainActivity extends AppCompatActivity {
                 rxDayuan.getClass("软件1632","2017-2018-2-1");
             }
         });
+
+        test_loadYearColleg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<YearCollege.DataBean.CollegesBean> list = dayuanDailyDatabase.loadYearCollege();
+                for (YearCollege.DataBean.CollegesBean collegesBean : list) {
+                    //Log.d(mainTag, "Id: "+collegesBean.getId() + "  College: " + collegesBean.getCollege());
+                }
+            }
+        });
+
+        test_loadMajor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<Major.DataBean> list = dayuanDailyDatabase.loadMajor(12);
+                for(Major.DataBean dataBean : list){
+                    Log.d(mainTag, "Id: "+dataBean.getId() + "  College_id : " + dataBean.getCollegeId()+"  Major: "+dataBean.getMajor());
+
+                }
+            }
+        });
+
+        test_loadClassName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<String> s = dayuanDailyDatabase.loadClassName(2016,"工业设计");
+                for(String s1: s){
+//                    Log.d(mainTag, "class: " + s1);
+                }
+            }
+        });
+        test_loadClass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = dayuanDailyDatabase.loadSchedules(DayuanDailyDatabase.TYPE_GET_SUB_TIME,2465,5,1);
+                Log.d(mainTag, "class_name is : " + s);
+            }
+        });
     }
 
     private void initId() {
+        test_loadClass = findViewById(R.id.test_loadClass);
+        test_loadYearColleg = findViewById(R.id.test_loadYearColleg);
         rxDayuan = new RxDayuan(this);
         test_YearCollege = findViewById(R.id.test_YearCollege);
         test_Major = findViewById(R.id.test_Major);
         test_classname = findViewById(R.id.test_classname);
         test_Class = findViewById(R.id.test_Class);
+        test_loadMajor = findViewById(R.id.test_loadMajor);
+        test_loadClassName = findViewById(R.id.test_loadClassName);
+        dayuanDailyDatabase = DayuanDailyDatabase.getInstance(this);
     }
 }
